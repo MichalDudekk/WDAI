f222c902e5fd6d1d13b1f2c38f223de826a5b12a
Michal Dudek <michaldudek.contact@gmail.com>
2025-10-23 19:16:18 +0200

    Add main-content functionality

--- projekt1/index.js
+++ projekt1/index.js
@@ -1,3 +1,69 @@
-setTimeout(() => {
-    console.log("Test JS");
-}, 4000);
+async function searchMovies(query) {
+    if (query === "") {
+        return { results: [] };
+    }
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+    const options = {
+        method: "GET",
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(
+                `Błąd sieci: Status ${response.status} ${response.statusText}`
+            );
+        }
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error(err);
+        return { results: [] };
+    }
+}
+
+let prevQuery = "";
+const API_KEY = "TOP_SECRET";
+
+async function handleSearchInput() {
+    const query = document.getElementById("searchInput").value;
+    if (query !== prevQuery) {
+        prevQuery = query;
+        const json = await searchMovies(query);
+        console.log(json);
+        const cardsContainer = document.getElementById("movieCards");
+        cardsContainer.innerHTML = "";
+
+        json.results.map((movie) => {
+            const card = document.createElement("div");
+            card.className = "card";
+            const img = document.createElement("img");
+            img.src = movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                : "temp.png";
+            const bottom = document.createElement("div");
+            bottom.className = "card-bottom";
+            const title = document.createElement("div");
+            title.className = "title";
+            title.innerText = `${movie.title} ⭐ ${
+                Math.round(movie.vote_average * 10) / 10
+            }`;
+            const bookmark = document.createElement("div");
+            bookmark.className = "bookmark";
+            bookmark.innerText = "🧾";
+
+            bottom.appendChild(title);
+            bottom.appendChild(bookmark);
+            card.appendChild(img);
+            card.appendChild(bottom);
+            cardsContainer.appendChild(card);
+        });
+    }
+    setTimeout(handleSearchInput, 1000);
+}
+
+handleSearchInput();
